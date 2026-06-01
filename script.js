0d2a14280602491fa2dff65236af21301a4b4b9f
Lucas Pachieri <ptorrelucas@gmail.com>
2026-06-01 04:22:27 -0300

    sexto commit do JS e fim do projeto

--- script.js
+++ script.js
@@ -71,4 +71,16 @@ function limpar() {
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 })
+
+
+gorjetaInput.value = ""
+
+pessoasInput.value = ""
+
+document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+conta = 0
+porcentagem = 0
+pessoas = 0
 }
